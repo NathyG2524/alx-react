68cf6adc437acc37fb5dc530bf1eddd4d976c437
NathyG2524 <nathygirma2524@gmail.com>
2022-07-29 08:47:39 +0300

    Test the new components

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySection from './BodySection';
+
+
+const wrapper = shallow(<BodySection title="test title">
+                          <p>test children node</p>
+                        </BodySection>)
+describe('BodySection component', () => {
+  it('renders correctly', () => {
+     expect(wrapper.containsMatchingElement(<h2>test title</h2>)).toEqual(true)
+     expect(wrapper.containsMatchingElement(<p>test children node</p>)).toEqual(true)
+  })
+})
