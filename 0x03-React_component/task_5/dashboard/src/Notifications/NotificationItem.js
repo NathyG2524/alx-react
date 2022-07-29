9d0916eb2b0965b5809fb4c23994859bd5d3db62
NathyG2524 <nathygirma2524@gmail.com>
2022-07-29 09:15:05 +0300

    Declare a pure component

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      // console.log(props.key)
+      //  (props.value) ? 
+      //  (<li data-notification-type={props.type}  onClick={()=>props.markAsRead(props.id)}>{props.value} </li> )
+      //  :
+      // (<li data-notification-type={props.type} dangerouslySetInnerHTML={props.html} onClick={()=>props.markAsRead(props.id)}></li>)
+      <li data-notification-type={props.type} dangerouslySetInnerHTML={props.html}
+      onClick={()=>props.markAsRead(props.id)}>{props.value}</li>
+      )
+  }
+
+};
+
+
+NotificationItem.defaultProps = {
+  type: "default"
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({__html: PropTypes.string}),
+  value: PropTypes.string
+}
+
+export default NotificationItem
