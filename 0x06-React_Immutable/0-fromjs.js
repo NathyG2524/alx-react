1e9140a9e5e1359984c132842664e23126ca6b8a
NathyG2524 <nathygirma2524@gmail.com>
2022-08-03 21:36:19 +0300

    [fix] eslint

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -2,9 +2,9 @@
 function getImmutableObject that accepts object as a parameter and
 converts it into an immutable Map using fromJS of the Immutable.js library
 */
-import {  fromJS } from 'immutable';
+import { fromJS } from 'immutable';
 
-export default function getImmutableObject (object){
+export default function getImmutableObject(object) {
     const newObj = fromJS(object)
     return newObj
 }
