fe8eb262d58f68e74c6cd343af3f4cfc29f00da7
NathyG2524 <nathygirma2524@gmail.com>
2022-08-03 21:31:25 +0300

    [Add]: comment

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,3 +1,7 @@
+/*
+function getImmutableObject that accepts object as a parameter and
+converts it into an immutable Map using fromJS of the Immutable.js library
+*/
 import {  fromJS } from 'immutable';
 
 export default function getImmutableObject (object){
