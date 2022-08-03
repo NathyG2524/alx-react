b5b051af935d8d608a8278769e53500ec0112c9a
NathyG2524 <nathygirma2524@gmail.com>
2022-08-03 23:52:46 +0300

    [fix] eslint

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -9,17 +9,17 @@ If two values are the same, page2 values should be used.
 Requirements:
 Use list and map from the Immutable.js library
 */
-import { List,  Map } from "immutable";
+import { List, Map } from 'immutable';
 
-export function concatElements (page1, page2) {
-    const page1list = List(page1)
-    const page2list = List(page2)
-    const all = page1list.concat(page2list)
-    return all
+export function concatElements(page1, page2) {
+  const page1list = List(page1);
+  const page2list = List(page2);
+  const all = page1list.concat(page2list);
+  return all;
 }
-export function mergeElements (page1, page2) {
-    const page1map = Map(page1)
-    const page2map = Map(page2)
+export function mergeElements(page1, page2) {
+  const page1map = Map(page1);
+  const page2map = Map(page2);
 
-    return page1map.merge(page2map)
+  return page1map.merge(page2map);
 }
