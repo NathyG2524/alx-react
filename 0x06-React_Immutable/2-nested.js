401db745306abfcd5401f7233072f24c0f719eea
NathyG2524 <nathygirma2524@gmail.com>
2022-08-03 22:24:40 +0300

    [fix] eslint style

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,14 +1,9 @@
-import { fromJS } from "immutable"
+// The first argument is a plain object
+// The second one is an array containing a list of a path to some key in the object
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    const obj = fromJS(object)
-    const name = obj.getIn(array)
-    return name
+  const obj = fromJS(object);
+  const name = obj.getIn(array);
+  return name;
 }
-
-console.log(accessImmutableObject({
-    name: {
-         first: "Guillaume",
-         last: "Salva"
-    }
-}, ['name', 'first']))
