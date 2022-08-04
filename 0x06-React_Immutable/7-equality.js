024a7ea3665ca9e27eeb124776743688257a3c1a
NathyG2524 <nathygirma2524@gmail.com>
2022-08-04 23:01:59 +0300

    [fix]: eslint style

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -3,6 +3,8 @@ Create a function named areMapsEqual
 It accepts two arguments map1 and map2. Both are Immutable.js Maps
 It should return true if the Maps are equal
 */
+import { is } from 'immutable';
+
 export default function areMapsEqual(map1, map2) {
-    return map1.equals(map2);
+  return is(map1, map2);
 }
