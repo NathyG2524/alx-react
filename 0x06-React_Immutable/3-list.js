f8fbe37004d58b694d7bf6f0b6cfe1be6d17f90f
NathyG2524 <nathygirma2524@gmail.com>
2022-08-04 23:04:28 +0300

    [fix]: style eslint

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,4 +1,5 @@
-// getListObject accepts an array as parameter and converts it into an immutable List using the Immutable.js library
+// getListObject accepts an array as parameter and
+// converts it into an immutable List using the Immutable.js library
 // addElementToList accepts two arguments: first one is a List and second one is a string
 // append the string to the list and return the list
 import { List } from 'immutable';
