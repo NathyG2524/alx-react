4a4cb2057d1b0e495a0cb7e4cd69370d97165336
NathyG2524 <nathygirma2524@gmail.com>
2022-08-15 21:56:06 +0300

    Add: Use Immutable for the UI Reducer

---
+++ 0x08-React_Redux_action_creator+normalizr/task_1/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,29 @@
+import { LOGIN_FAILURE, 
+    LOGIN_SUCCESS,
+    DISPLAY_NOTIFICATION_DRAWER,
+LOGOUT,
+HIDE_NOTIFICATION_DRAWER } from "../action/uiActionTypes";
+import {Map} from 'immutable'
+
+const initial_state = {
+    isNotificationDrawerVisible: false,
+    isUserLoggerIn: true,
+    user: {}
+};
+export const uiReducer = (state = Map(initial_state), action) => {
+    switch(action.type) {
+        case DISPLAY_NOTIFICATION_DRAWER:
+            return state.set("isNotificationDrawerVisible", true)
+        case HIDE_NOTIFICATION_DRAWER:
+            return state.set("isNotificationDrawerVisible", false)
+            
+        case LOGIN_SUCCESS:
+            return state.set("isUserLoggerIn", true)            
+        case LOGIN_FAILURE:
+            return state.set("isUserLoggerIn", false)
+            
+        case LOGOUT:
+            return state.set("isUserLoggerIn", false)
+            
+    }
+}
